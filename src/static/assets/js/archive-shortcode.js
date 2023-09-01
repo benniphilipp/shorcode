e106374743c2e753c812b7e19ade03c49be81147
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-09-01 21:54:31 +0200

    Eidte Step3 Shorcode

--- src/static/assets/js/archive-shortcode.js
+++ src/static/assets/js/archive-shortcode.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
 
+    const getCookie =(name) => {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    const csrftoken = getCookie('csrftoken');
+
     //Archive function
     $('#archive-btn').on('click', function(event){
         event.preventDefault();
 
         var dataArchive = jQuery(this).attr('data-archive');
-
-        //form fuc disabled
-        disabledTextInput();
-
-        //Overlay
-        overlayReady();
         
         $.ajax({
             type: 'POST',
@@ -22,7 +33,6 @@ $(document).ready(function(){
             success: function(response){
                 console.log(response);
                 setTimeout(()=>{
-                    window.location.reload();
                     $('#overlay').removeClass('overlay-active');
                     $('#overlay-open').removeClass("overlay-open"); 
                 }, 1000);
--- src/static/assets/js/shortcode-neu.js
+++ src/static/assets/js/shortcode-neu.js
@@ -88,6 +88,32 @@ $(document).ready(function(){
 
     });
 
+
+    /* Alert Box Close */
+    function clearContent() {
+        setTimeout(function() {
+            $('#toast-alert').html('');
+        }, 4000);
+    }
+
+    /* Alert Box */
+    function ls_toast(parmToast){
+        $('#toast-alert').html(`
+            <div class="ls-toast" id="ls-toas">
+                <div class="ls-toas-header d-flex justify-content-start align-items-center px-2 py-2">
+                    <svg class="bd-placeholder-img rounded me-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#007aff"></rect></svg>
+                    <span><b>Meldung</b></span>
+                    <i class="fa-solid fa-xmark ms-auto"></i>
+                </div>
+                <hr>
+                <div class="ls-toas-body p-2">
+                    ${parmToast}
+                </div>
+            </div>
+        `);
+        clearContent();
+    };
+
     const url_destination = document.getElementById('id_url_destination');
     const url_titel = document.getElementById('id_url_titel');
     const url_medium = document.getElementById('id_url_medium');
@@ -99,86 +125,86 @@ $(document).ready(function(){
     const url_creator = document.getElementById('url_creator');
     const idShort = document.getElementById('id_shortcode');
 
-        /** Update Shorcode View **/
-        $('#update-form-shortcode').on('click', function(event){
-            event.preventDefault();
-    
-            var idShortcode = $('#update-shortcode-url').val();
-            const url_update = url_view_update + '/shortcode/update/' + idShortcode + '/';
-            $('#archive-btn').attr('data-archive', idShortcode);
-    
-            const fd = new FormData();
-            fd.append('csrfmiddlewaretoken', csrf[0].value)
-            fd.append('url_destination', url_destination.value);
-            fd.append('url_titel', url_titel.value);
-            fd.append('url_source', url_source.value);
-            fd.append('url_medium', url_medium.value);
-            fd.append('url_term', url_term.value);
-            fd.append('url_campaign', url_campaign.value);
-            fd.append('url_creator', url_creator.value);
-            fd.append('url_content', url_content.value);
-            fd.append('shortcode_id', idShort.value);
-    
-            const selectedTags = [];
-            $('input[name="tags"]input[type="checkbox"]:checked').each(function() {
-                selectedTags.push($(this).val());
-    
-            });
-    
-            fd.append('tags', selectedTags.join(','));
-    
-            $.ajax({
-                type: 'POST',
-                url: url_update,
-                data: fd,
-                enctype: 'multipart/form-data',
-                success: function(response){
-    
-                    //form fuc disabled
-                    $('.disabled-func').each(function() {
-                        $(this).find('input[type=text]').attr('disabled', 'disabled');
-                    });
-    
-                    //Overlay
-                    $('#overlay').addClass('overlay-active');
-                    var dataImage = jQuery('#overlay').attr('data-image');
-                    $('#overlay').html("<div class=\"overlay-body\"><img src='"+dataImage+"' width=\"60\" height=\"60\"><span>Warten...</span></div>")
-    
-                    //Reset fields
-                    $('#id_url_destination').val('')
-                    $('#id_url_titel').val('')
-                    $('#id_url_medium').val('')
-                    $('#id_url_source').val('')
-                    $('#id_url_term').val('')
-                    $('#id_url_titel').val('')
-                    $('#id_url_campaign').val('')
-                    $('#id_url_content').val('')
-                    $('#id_shortcode').val('')
-    
-                    const tagsCheckboxes = $('input[name="tags"][type="checkbox"]');
-                    tagsCheckboxes.each(function(index, checkbox) {
-                        const tagValue = parseInt($(checkbox).val());
-    
-                        $(checkbox).prop('checked', '');
-                    });
-    
-                    // //Alert
-                    ls_toast(response.success);
-    
-                    setTimeout(()=>{
-                        window.location.reload();
-                        $('#overlay').removeClass('overlay-active');
-                    }, 2000);
-    
-                },
-                error: function(error){
-                    console.log(error);
-                },
-                cache: false,
-                contentType: false,
-                processData: false,
-            })
-        });      
+    /** Update Shorcode View **/
+    $('#update-form-shortcode').on('click', function(event){
+        event.preventDefault();
+
+        var idShortcode = $('#update-shortcode-url').val();
+        const url_update = '/shortcode/update/' + idShortcode + '/';
+        $('#archive-btn').attr('data-archive', idShortcode);
+
+        const fd = new FormData();
+        fd.append('csrfmiddlewaretoken', csrf[0].value)
+        fd.append('url_destination', url_destination.value);
+        fd.append('url_titel', url_titel.value);
+        fd.append('url_source', url_source.value);
+        fd.append('url_medium', url_medium.value);
+        fd.append('url_term', url_term.value);
+        fd.append('url_campaign', url_campaign.value);
+        fd.append('url_creator', url_creator.value);
+        fd.append('url_content', url_content.value);
+        fd.append('shortcode_id', idShort.value);
+
+        const selectedTags = [];
+        $('input[name="tags"]input[type="checkbox"]:checked').each(function() {
+            selectedTags.push($(this).val());
+
+        });
+
+        fd.append('tags', selectedTags.join(','));
+
+        $.ajax({
+            type: 'POST',
+            url: url_update,
+            data: fd,
+            enctype: 'multipart/form-data',
+            success: function(response){
+
+                //form fuc disabled
+                $('.disabled-func').each(function() {
+                    $(this).find('input[type=text]').attr('disabled', 'disabled');
+                });
+
+                //Overlay
+                // $('#overlay').addClass('overlay-active');
+                // var dataImage = jQuery('#overlay').attr('data-image');
+                // $('#overlay').html("<div class=\"overlay-body\"><img src='"+dataImage+"' width=\"60\" height=\"60\"><span>Warten...</span></div>")
+
+                //Reset fields
+                $('#id_url_destination').val('')
+                $('#id_url_titel').val('')
+                $('#id_url_medium').val('')
+                $('#id_url_source').val('')
+                $('#id_url_term').val('')
+                $('#id_url_titel').val('')
+                $('#id_url_campaign').val('')
+                $('#id_url_content').val('')
+                $('#id_shortcode').val('')
+
+                const tagsCheckboxes = $('input[name="tags"][type="checkbox"]');
+                tagsCheckboxes.each(function(index, checkbox) {
+                    const tagValue = parseInt($(checkbox).val());
+
+                    $(checkbox).prop('checked', '');
+                });
+
+                // //Alert
+                ls_toast(response.success);
+
+                setTimeout(()=>{
+                    window.location.reload();
+                    $('#overlay').removeClass('overlay-active');
+                }, 2000);
+
+            },
+            error: function(error){
+                console.log(error);
+            },
+            cache: false,
+            contentType: false,
+            processData: false,
+        })
+    });      
         
         
 
